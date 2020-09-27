0a5130dd28825caaa7c9d9016c658c2b300cfaf7
Annie Pennell <annie@coverhound.com>
2020-09-27 12:59:22 -0700

    Add separate webpack dev and prod configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  mode: 'development',
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -17,6 +20,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
     }),
   ],
 };
