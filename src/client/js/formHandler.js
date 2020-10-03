4e48f4fc31548dc6137247e370c1075844f7743c
Annie Pennell <annie@coverhound.com>
2020-10-02 17:30:58 -0700

    Update the DOM with results
    WIP: need to change info displayed

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,16 @@
+// Submit URL to API to process with meaningcloud sentiment analysis
 const getProcessedResults = async (url) => {
   console.log('url', url);
+  /**
+   * RESULTS object structure (on success):
+   * agreement: "DISAGREEMENT"
+   * confidence: "91"
+   * irony: "NONIRONIC"
+   * model: "general_en"
+   * score_tag: "P"
+   * subjectivity: "SUBJECTIVE"
+   * status: {}
+   */
 
   const resp = await fetch('/api', {
     method: 'POST',
@@ -16,19 +27,57 @@ const getProcessedResults = async (url) => {
   return resp;
 };
 
+// Update UI to indicate loading status on submit
+const setLoading = (isLoading, url) => {
+  const submitButton = document.querySelector('button[type="submit"]');
+  submitButton.textContent = isLoading ? 'Processing...' : 'Process';
+  submitButton.disabled = isLoading;
+
+  const resultsHeader = document.querySelector('#results-header');
+  resultsHeader.textContent = isLoading
+    ? `Analyzing '${url}'`
+    : `Results from analysis of '${url}'`;
+};
+
+// Parse results and populate the UI
+const buildResultsUI = (results) => {
+  const resultsDisplay = document.createDocumentFragment();
+  if (results.score_tag) {
+    // TODO: add additional result information and better structure DOM manipulation
+    const resultMain = document.createElement('h3');
+    resultMain.textContent = `This source was found to be overall ${results.score_tag}.`;
+    resultsDisplay.appendChild(resultMain);
+  } else {
+    const resultsEmpty = document.createElement('p');
+    resultsDisplay.textContent = 'We were unable to process this source.';
+    resultsDisplay.appendChild(resultsEmpty);
+  }
+
+  // FIXME: better setup for updating DOM display (not appendChild)
+  const resultsSection = document.querySelector('#results');
+  resultsSection.appendChild(resultsDisplay);
+};
+
 const handleSubmit = async (e) => {
   e.preventDefault();
 
-  // Get value from form field
-  const input = document.querySelector('#url').value;
+  const inputField = document.querySelector('#url');
+  const url = inputField.value;
+  setLoading(true, url);
 
-  // Validate value
+  // TODO: Validate value
+  const valid = true;
 
-  // If valid, call /api, then replace content with response
-  const results = await getProcessedResults(input);
-  console.log('results are: ', results);
+  if (valid) {
+    const results = await getProcessedResults(url);
+    console.log('results are: ', results);
+    buildResultsUI(results);
+    inputField.value = '';
+  } else {
+    // TODO: If not valid, add error message under form field
+  }
 
-  // If not valid, add error message under form field
+  setLoading(false, url);
 };
 
 export default handleSubmit;
